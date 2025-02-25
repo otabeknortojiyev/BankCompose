b9614a31dbb7be251a2710e974206d9829f66ec7
Отабек Нортожиев <otashmotash@gmail.com>
2025-02-25 23:22:03 +0500

    some changes

--- app/src/main/java/uz/gita/otabek/bankauthcompose/screens/tabs/exchange/ExchangeTab.kt
+++ app/src/main/java/uz/gita/otabek/bankauthcompose/screens/tabs/exchange/ExchangeTab.kt
@@ -1,20 +1,10 @@
 package uz.gita.otabek.bankauthcompose.screens.tabs.exchange
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import java.nio.file.WatchEvent
 
 object ExchangeTab : Tab {
     private fun readResolve(): Any = ExchangeTab
